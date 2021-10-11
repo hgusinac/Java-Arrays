617cdc03959b11c956b9d340c20be927e7ff1d8d
Haris Gusinac <hgusinac@gmail.com>
2021-10-11 15:55:50 +0200

    Initial commit

---
+++ src/com/harisgusinac/Main.java
@@ -0,0 +1,12 @@
+package com.harisgusinac;
+
+import java.awt.*;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+String message = "Hello World" + "!!";
+        System.out.println(message.indexOf("w"));
+    }
+}
